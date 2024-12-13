fae7a0eb7fe5747c546a9aca1393e9093e53baa8
rlebras <ronanlb@allenai.org>
2024-12-13 14:36:14 -0800

    run

--- gantry_cmd.sh
+++ gantry_cmd.sh
@@ -3,6 +3,7 @@
 # Set the workspace and cluster variables
 WORKSPACE="ronanlb-workspace-internal"
 CLUSTER="ai2/mosaic-cirrascale"
+BUDGET="ai2/oe-eval"
 
 # Run the gantry command
-gantry run --workspace "$WORKSPACE" --cluster "$CLUSTER" -- python run.py
+gantry run --workspace "$WORKSPACE" --cluster "$CLUSTER" -- python run.py --budget "$BUDGET"
