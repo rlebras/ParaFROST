c6dc3fca3e120e1f4591c0a417d5decc0efa8714
rlebras <ronanlb@allenai.org>
2025-02-15 13:36:25 -0800

    glucose

--- gantry_ramsey.sh
+++ gantry_ramsey.sh
@@ -6,7 +6,7 @@ CLUSTER="ai2/saturn-cirrascale"
 BUDGET="ai2/oe-eval"
 
 # Run the gantry command
-for n in 17
+for n in 102
 do
-  gantry run --name "r_$n" --preemptible --gpus 1 --workspace "$WORKSPACE" --cluster "$CLUSTER" --budget "$BUDGET" -- python ramsey.py -n "$n" -s 4 -t 4
+  gantry run --name "r_$n" --preemptible --gpus 1 --workspace "$WORKSPACE" --cluster "$CLUSTER" --budget "$BUDGET" -- python ramsey.py -n "$n" -s 6 -t 6
 done
